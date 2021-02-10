90c3524f728e27b6369da89027a3956d7c0ef7a0
bmpschneider <brunopschneider@gmail.com>
2021-02-09 21:29:29 -0300

    Update Script Install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,9 @@
-sudo apt-get update
-sudo apt-get upgrade
 sudo apt install nodejs
 sudo apt install wget
 wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | bash
 source ~/.profile
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 nvm install 14.15.4
-cd url-shortener
 npm install
 npx sequelize-cli db:migrate
 sudo iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 5000
