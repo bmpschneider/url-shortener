4386f31b407d54061f8547f8453f242cd332adc6
bmpschneider <brunopschneider@gmail.com>
2021-02-09 21:16:42 -0300

    Update Script

--- install.sh
+++ install.sh
@@ -8,24 +8,6 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 nvm install 14.15.4
 cd url-shortener
 npm install
-sudo apt install mysql-server
-sudo mysql_secure_installation
-root
-root
-
-
-
-
-sudo mysql
-ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'root';
-FLUSH PRIVILEGES;
-exit;
-sudo mysql -uroot -proot
-CREATE USER urlshortener@localhost IDENTIFIED BY 'urlshortener';
-CREATE DATABASE urlshortener;
-GRANT ALL ON urlshortener.* TO urlshortener@localhost IDENTIFIED BY 'urlshortener';
-FLUSH PRIVILEGES;
-exit;
 npx sequelize-cli db:migrate
 sudo iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 5000
 sh start.sh
